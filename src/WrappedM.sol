b76b451f73692dafb060e90fafaa0ef23d4526ea
Michael De Luca <michael.deluca@circle-free.com>
2024-06-12 09:37:26 -0400

    draft

--- src/WrappedM.sol
+++ src/WrappedM.sol
@@ -10,8 +10,6 @@ import { IMTokenLike } from "./interfaces/IMTokenLike.sol";
 import { IWrappedM } from "./interfaces/IWrappedM.sol";
 import { IRegistrarLike } from "./interfaces/IRegistrarLike.sol";
 
-// TODO: Allow willing accounts to block the activate of claim delegates for this account.
-
 contract WrappedM is IWrappedM, ERC20Extended {
     type Balance is uint256;
 
@@ -19,12 +17,12 @@ contract WrappedM is IWrappedM, ERC20Extended {
 
     uint56 internal constant _EXP_SCALED_ONE = 1e12;
 
-    bytes32 internal constant _ALLOCATORS_LIST = "wm_allocators";
     bytes32 internal constant _EARNERS_LIST = "earners";
-    bytes32 internal constant _EARNING_DELEGATE_PREFIX = "earning_delegate";
+    bytes32 internal constant _CLAIM_DESTINATION_PREFIX = "claim_destination";
 
     address public immutable mToken;
     address public immutable registrar;
+    address public immutable vault;
 
     uint240 public totalEarningSupply;
 
@@ -32,8 +30,6 @@ contract WrappedM is IWrappedM, ERC20Extended {
     uint128 public indexOfNonEarningSupply;
 
     mapping(address account => Balance balance) internal _balances;
-
-    mapping(address account => address claimDelegate) public claimDelegateOf;
 
     /* ============ Modifiers ============ */
 
@@ -48,58 +44,28 @@ contract WrappedM is IWrappedM, ERC20Extended {
 
     /* ============ Interactive Functions ============ */
 
-    function activateClaimDelegate(address account_) external {
-        claimDelegateOf[account_] = _getEaringDelegate(account_);
-    }
-
     function claim() external returns (uint256 yield_) {
-        ( bool isEarning_, , ) = _unwrap(_balances[msg.sender]);
-
-        if (!isEarning_) revert NotEarning();
-
-        return _claimForEarner(msg.sender, currentMIndex());
+        return _claimForEarner(msg.sender);
     }
 
-    // TODO: Should anyone be allowed to do this? No harm, but legal implications?
     function claimForTotalNonEarningSupply() external returns (uint256 yield_) {
-        return _claimForTotalNonEarningSupply(currentMIndex());
+        return _claimForTotalNonEarningSupply();
     }
 
     function deposit(address destination_, uint256 amount_) external {
         IMTokenLike(mToken).transferFrom(msg.sender, address(this), amount_);
-
         _mint(destination_, UIntMath.safe240(amount_));
     }
 
     function withdraw(address destination_, uint256 amount_) external {
         IMTokenLike(mToken).transfer(destination_, amount_);
-
         _burn(msg.sender, UIntMath.safe240(amount_));
     }
 
-    // TODO: Could be replace with a transferFrom override, but transferFrom is not virtual.
-    function allocate(address recipient_, uint256 amount_) external {
-        if (!_isApprovedAllocator(msg.sender)) revert NotAllocator();
-
-        _transfer(address(this), recipient_, amount_);
-    }
-
-    function startEarning() external {
-        if (!_isApprovedEarner(msg.sender)) revert NotApprovedEarner();
-
-        _startEarning(msg.sender);
-    }
-
     function startEarning(address account_) external {
-        if (_getEaringDelegate(account_) != msg.sender) revert NotEarningDelegate();
-
         if (!_isApprovedEarner(account_)) revert NotApprovedEarner();
 
         _startEarning(account_);
-    }
-
-    function stopEarning() external {
-        _stopEarning(msg.sender);
     }
 
     function stopEarning(address account_) external {
@@ -111,7 +77,7 @@ contract WrappedM is IWrappedM, ERC20Extended {
     /* ============ View/Pure Functions ============ */
 
     function accruedYieldOf(address account_) public view returns (uint256 yield_) {
-        return _accruedYield(_balances[account_], currentMIndex());
+        return _accruedYield(_balances[account_]);
     }
 
     function accruedYieldOfEarningSupply() public view returns (uint256 yield_) {
@@ -122,7 +88,7 @@ contract WrappedM is IWrappedM, ERC20Extended {
     }
 
     function accruedYieldOfNonEarningSupply() public view returns (uint256 yield_) {
-        return _accruedYield(principalOfTotalNonEarningSupply, indexOfNonEarningSupply, currentMIndex());
+        return _accruedYield(principalOfTotalNonEarningSupply, indexOfNonEarningSupply);
     }
 
     function balanceOf(address account) external view returns (uint256 balance_) {
@@ -145,77 +111,74 @@ contract WrappedM is IWrappedM, ERC20Extended {
 
     /* ============ Internal Interactive Functions ============ */
 
-    function _claimForEarner(address account_, uint128 currentIndex_) internal returns (uint240 yield_) {
-        ( , uint128 index_, uint256 rawBalance_) = _unwrap(_balances[account_]);
+    function _claimForEarner(address account_) internal returns (uint240 yield_) {
+        ( bool isEarner_, uint128 index_, uint256 rawBalance_) = _unwrap(_balances[account_]);
 
-        if (index_ == currentIndex_) return 0;
+        if (!isEarner_) return 0;
 
-        yield_ = _accruedYield(uint112(rawBalance_), index_, currentIndex_);
+        yield_ = _accruedYield(uint112(rawBalance_), index_);
 
-        _balances[account_] = _wrap(true, currentIndex_, rawBalance_);
+        _balances[account_] = _wrap(true, currentMIndex(), rawBalance_);
 
-        if (yield_ != 0) {
-            emit Claim(account_, yield_);
-            emit Transfer(address(0), account_, yield_);
+        totalEarningSupply += yield_;
 
-            totalEarningSupply += yield_;
-        }
+        address claimDestination_ = _getClaimDestination(account_);
 
-        address claimDelegate_ = claimDelegateOf[account_];
-
-        if (claimDelegate_ != address(0) && claimDelegate_ != account_) {
+        if (claimDestination_ != address(0)) {
             // NOTE: Watch out for a long chain of delegations.
             // TODO: Maybe can be optimized since we know `account_` is an earner and already claimed.
-            _transfer(account_, claimDelegate_, yield_);
+            _transfer(account_, claimDestination_, yield_);
         }
     }
 
-    function _claimForTotalNonEarningSupply(uint128 currentIndex_) internal returns (uint240 yield_) {
-        yield_ = _accruedYield(principalOfTotalNonEarningSupply, indexOfNonEarningSupply, currentIndex_);
+    function _claimForTotalNonEarningSupply() internal returns (uint240 yield_) {
+        if (indexOfNonEarningSupply == currentMIndex()) return 0;
 
-        if (yield_ != 0) {
-            emit Claim(address(this), yield_);
-            emit Transfer(address(0), address(this), yield_);
-        }
+        yield_ = _accruedYield(principalOfTotalNonEarningSupply, indexOfNonEarningSupply);
 
         ( , , uint256 rawBalance_ ) = _unwrap(_balances[address(this)]);
 
         _balances[address(this)] = _wrap(false, 0, rawBalance_ + yield_);
-        indexOfNonEarningSupply = currentIndex_;
+        indexOfNonEarningSupply = currentMIndex();
+
+        address claimDestination_ = _getClaimDestination(address(this));
+
+        if (claimDestination_ != address(0)) {
+            // NOTE: Watch out for a long chain of delegations.
+            // TODO: Maybe can be optimized since we know `account_` is an earner and already claimed.
+            _transfer(address(this), claimDestination_, yield_);
+        }
     }
 
     function _burn(address account_, uint240 amount_) internal {
         ( bool isEarning_, , ) = _unwrap(_balances[account_]);
 
-        uint128 currentIndex_ = currentMIndex();
+        _claimForEarner(account_);
+        _claimForTotalNonEarningSupply();
 
         if (isEarning_) {
-            _claimForEarner(account_, currentIndex_);
-            _burnEarningAmount(account_, amount_, currentIndex_);
+            _burnEarningAmount(account_, amount_);
         } else {
-            _claimForTotalNonEarningSupply(currentIndex_);
-            _burnNonEarningAmount(account_, amount_, currentIndex_);
+            _burnNonEarningAmount(account_, amount_);
         }
-
-        emit Transfer(account_, address(0), amount_);
     }
 
     /// @dev Should only be called if indexOfNonEarningSupply is currentIndex_.
-    function _burnNonEarningAmount(address account_, uint240 amount_, uint128 currentIndex_) internal {
+    function _burnNonEarningAmount(address account_, uint240 amount_) internal {
         ( , , uint256 rawBalance_ ) = _unwrap(_balances[account_]);
 
         _balances[account_] = _wrap(false, 0, rawBalance_ - amount_);
-        principalOfTotalNonEarningSupply -= _getPrincipalAmountRoundedDown(amount_, currentIndex_);
+        principalOfTotalNonEarningSupply -= _getPrincipalAmountRoundedDown(amount_, currentMIndex());
     }
 
     /// @dev Should only be called if _balances[account_].index is currentIndex_.
-    function _burnEarningAmount(address account_, uint240 amount_, uint128 currentIndex_) internal {
+    function _burnEarningAmount(address account_, uint240 amount_) internal {
         ( , , uint256 rawBalance_ ) = _unwrap(_balances[account_]);
 
         _balances[account_] = _wrap(
             true,
-            currentIndex_,
-            rawBalance_ - _getPrincipalAmountRoundedUp(amount_, currentIndex_)
+            currentMIndex(),
+            rawBalance_ - _getPrincipalAmountRoundedUp(amount_, currentMIndex())
         );
 
         totalEarningSupply -= amount_;
@@ -224,34 +187,31 @@ contract WrappedM is IWrappedM, ERC20Extended {
     function _mint(address recipient_, uint240 amount_) internal {
         ( bool isEarning_, , ) = _unwrap(_balances[recipient_]);
 
-        uint128 currentIndex_ = currentMIndex();
+        _claimForEarner(recipient_);
+        _claimForTotalNonEarningSupply();
 
         if (isEarning_) {
-            _claimForEarner(recipient_, currentIndex_);
-            _mintEarningAmount(recipient_, amount_, currentIndex_);
+            _mintEarningAmount(recipient_, amount_);
         } else {
-            _claimForTotalNonEarningSupply(currentIndex_);
-            _mintNonEarningAmount(recipient_, amount_, currentIndex_);
+            _mintNonEarningAmount(recipient_, amount_);
         }
-
-        emit Transfer(address(0), recipient_, amount_);
     }
 
     /// @dev Should only be called if indexOfNonEarningSupply is currentIndex_.
-    function _mintNonEarningAmount(address recipient_, uint240 amount_, uint128 currentIndex_) internal {
+    function _mintNonEarningAmount(address recipient_, uint240 amount_) internal {
         ( , , uint256 rawBalance_ ) = _unwrap(_balances[recipient_]);
 
         _balances[recipient_] = _wrap(false, 0, rawBalance_ + amount_);
-        principalOfTotalNonEarningSupply += _getPrincipalAmountRoundedDown(amount_, currentIndex_);
+        principalOfTotalNonEarningSupply += _getPrincipalAmountRoundedDown(amount_, currentMIndex());
     }
 
     /// @dev Should only be called if _balances[recipient_].index is currentIndex_.
-    function _mintEarningAmount(address recipient_, uint240 amount_, uint128 currentIndex_) internal {
+    function _mintEarningAmount(address recipient_, uint240 amount_) internal {
         ( , , uint256 rawBalance_ ) = _unwrap(_balances[recipient_]);
 
-        uint112 principalAmount_ = _getPrincipalAmountRoundedDown(amount_, currentIndex_);
+        uint112 principalAmount_ = _getPrincipalAmountRoundedDown(amount_, currentMIndex());
 
-        _balances[recipient_] = _wrap(true, currentIndex_, rawBalance_ + principalAmount_);
+        _balances[recipient_] = _wrap(true, currentMIndex(), rawBalance_ + principalAmount_);
 
         totalEarningSupply += amount_;
     }
@@ -261,15 +221,11 @@ contract WrappedM is IWrappedM, ERC20Extended {
 
         if (isEarning_) return;
 
-        emit StartEarning(account_);
+        _claimForTotalNonEarningSupply();
 
-        uint128 currentIndex_ = currentMIndex();
+        uint112 principalAmount_ = _getPrincipalAmountRoundedDown(uint240(rawBalance_), currentMIndex());
 
-        _claimForTotalNonEarningSupply(currentIndex_);
-
-        uint112 principalAmount_ = _getPrincipalAmountRoundedDown(uint240(rawBalance_), currentIndex_);
-
-        _balances[account_] = _wrap(true, currentIndex_, principalAmount_);
+        _balances[account_] = _wrap(true, currentMIndex(), principalAmount_);
 
         principalOfTotalNonEarningSupply -= principalAmount_;
         totalEarningSupply += uint240(rawBalance_);
@@ -280,12 +236,8 @@ contract WrappedM is IWrappedM, ERC20Extended {
 
         if (!isEarning_) return;
 
-        uint128 currentIndex_ = currentMIndex();
-
-        _claimForEarner(account_, currentIndex_);
-        _claimForTotalNonEarningSupply(currentIndex_);
-
-        emit StopEarning(account_);
+        _claimForEarner(account_);
+        _claimForTotalNonEarningSupply();
 
         ( , uint128 index_, uint256 rawBalance_) = _unwrap(_balances[account_]);
 
@@ -298,8 +250,6 @@ contract WrappedM is IWrappedM, ERC20Extended {
     }
 
     function _transfer(address sender_, address recipient_, uint256 amount_) internal override {
-        emit Transfer(sender_, recipient_, amount_);
-
         uint240 safeAmount_ = UIntMath.safe240(amount_);
 
         ( bool senderIsEarning_, , uint256 senderRawBalance_ ) = _unwrap(_balances[sender_]);
@@ -312,47 +262,38 @@ contract WrappedM is IWrappedM, ERC20Extended {
             return;
         }
 
-        uint128 currentIndex_ = currentMIndex();
+        _claimForEarner(sender_);
+        _claimForEarner(recipient_);
+        _claimForTotalNonEarningSupply();
 
         if (senderIsEarning_ && recipientIsEarning_) {
-            _claimForEarner(sender_, currentIndex_);
-            _claimForEarner(recipient_, currentIndex_);
-
             ( , , senderRawBalance_ ) = _unwrap(_balances[sender_]);
             ( , , recipientRawBalance_ ) = _unwrap(_balances[recipient_]);
 
-            uint112 principalAmount_ = _getPrincipalAmountRoundedDown(safeAmount_, currentIndex_);
+            uint112 principalAmount_ = _getPrincipalAmountRoundedDown(safeAmount_, currentMIndex());
 
-            _balances[sender_] = _wrap(true, currentIndex_, senderRawBalance_ - principalAmount_);
-            _balances[recipient_] = _wrap(true, currentIndex_, recipientRawBalance_ + principalAmount_);
+            _balances[sender_] = _wrap(true, currentMIndex(), senderRawBalance_ - principalAmount_);
+            _balances[recipient_] = _wrap(true, currentMIndex(), recipientRawBalance_ + principalAmount_);
         } else if (senderIsEarning_) {
-            _claimForEarner(sender_, currentIndex_);
-            _claimForTotalNonEarningSupply(currentIndex_);
-            _burnEarningAmount(sender_, safeAmount_, currentIndex_);
-            _mintNonEarningAmount(recipient_, safeAmount_, currentIndex_);
+            _burnEarningAmount(sender_, safeAmount_);
+            _mintNonEarningAmount(recipient_, safeAmount_);
         } else {
-            _claimForEarner(recipient_, currentIndex_);
-            _claimForTotalNonEarningSupply(currentIndex_);
-            _burnNonEarningAmount(sender_, safeAmount_, currentIndex_);
-            _mintEarningAmount(recipient_, safeAmount_, currentIndex_);
+            _burnNonEarningAmount(sender_, safeAmount_);
+            _mintEarningAmount(recipient_, safeAmount_);
         }
     }
 
     /* ============ Internal View/Pure Functions ============ */
 
-    function _accruedYield(Balance balance_, uint128 currentIndex_) internal pure returns (uint240 yield_) {
+    function _accruedYield(Balance balance_) internal view returns (uint240 yield_) {
         ( bool isEarning_, uint128 index_, uint256 rawBalance_) = _unwrap(balance_);
 
-        return isEarning_ ? _accruedYield(uint112(rawBalance_), index_, currentIndex_) : 0;
+        return isEarning_ ? _accruedYield(uint112(rawBalance_), index_) : 0;
     }
 
-    function _accruedYield(
-        uint112 principalAmount_,
-        uint128 index_,
-        uint128 currentIndex_
-    ) internal pure returns (uint240 yield_) {
-        // TODO: Compare with `_getPresentAmountRoundedDown(principalAmount_, currentIndex_) - _getPresentAmountRoundedDown(principalAmount_, index_)`
-        return _getPresentAmountRoundedDown(principalAmount_, currentIndex_ - index_);
+    function _accruedYield(uint112 principalAmount_, uint128 index_) internal view returns (uint240 yield_) {
+        // TODO: Compare with `_getPresentAmountRoundedDown(principalAmount_, currentMIndex()) - _getPresentAmountRoundedDown(principalAmount_, index_)`
+        return _getPresentAmountRoundedDown(principalAmount_, currentMIndex() - index_);
     }
 
     function _isApprovedEarner(address account_) internal view returns (bool isApproved_) {
@@ -361,16 +302,12 @@ contract WrappedM is IWrappedM, ERC20Extended {
         return (account_ != address(this)) && IRegistrarLike(registrar).listContains(_EARNERS_LIST, account_);
     }
 
-    function _isApprovedAllocator(address account_) internal view returns (bool isApproved_) {
-        return IRegistrarLike(registrar).listContains(_ALLOCATORS_LIST, account_);
-    }
-
-    function _getEaringDelegate(address account_) internal view returns (address earningDelegate_) {
+    function _getClaimDestination(address account_) internal view returns (address destination_) {
         return
             address(
                 uint160(
                     uint256(
-                        IRegistrarLike(registrar).get(keccak256(abi.encode(_EARNING_DELEGATE_PREFIX, account_)))
+                        IRegistrarLike(registrar).get(keccak256(abi.encode(_CLAIM_DESTINATION_PREFIX, account_)))
                     )
                 )
             );
