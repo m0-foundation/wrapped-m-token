e48830b4b2143fd62a67c011590d1713ec0d117a
Pierrick Turelier <pierrick@turelier.com>
2024-07-15 10:52:02 +0200

    fix(Proxy): gas optimized _IMPLEMENTATION_SLOT

--- src/Proxy.sol
+++ src/Proxy.sol
@@ -4,25 +4,21 @@ pragma solidity 0.8.23;
 
 contract Proxy {
     /// @dev Storage slot with the address of the current factory. `keccak256('eip1967.proxy.implementation') - 1`.
-    bytes32 private constant _IMPLEMENTATION_SLOT =
-        bytes32(0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc);
+    uint256 private constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     constructor(address implementation_) {
         if (implementation_ == address(0)) revert();
 
-        bytes32 slot_ = _IMPLEMENTATION_SLOT;
-
         assembly {
-            sstore(slot_, implementation_)
+            sstore(_IMPLEMENTATION_SLOT, implementation_)
         }
     }
 
     fallback() external payable virtual {
-        bytes32 slot_ = _IMPLEMENTATION_SLOT;
         bytes32 implementation_;
 
         assembly {
-            implementation_ := sload(slot_)
+            implementation_ := sload(_IMPLEMENTATION_SLOT)
         }
 
         assembly {
