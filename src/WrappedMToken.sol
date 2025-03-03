5aa9684e0476eb7ec2c459afcee2d290d119372d
toninorair <antonina.norair@gmail.com>
2025-03-02 22:51:07 -0500

    Rebase on main v2 branch (#115)
    Co-authored-by: Michael De Luca <michael.deluca@circle-free.com>

--- src/WrappedMToken.sol
+++ src/WrappedMToken.sol
@@ -95,8 +95,11 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
     /// @dev Mapping of accounts to their respective `AccountInfo` structs.
     mapping(address account => Account balance) internal _accounts;
 
-    /// @dev Array of indices at which earning was enabled or disabled.
-    uint128[] internal _enableDisableEarningIndices;
+    /// @inheritdoc IWrappedMToken
+    uint128 public enableMIndex;
+
+    /// @inheritdoc IWrappedMToken
+    uint128 public disableIndex;
 
     mapping(address account => address claimRecipient) internal _claimRecipients;
 
@@ -195,17 +198,9 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
         if (!_isThisApprovedEarner()) revert NotApprovedEarner(address(this));
         if (isEarningEnabled()) revert EarningIsEnabled();
 
-        // NOTE: This is a temporary measure to prevent re-enabling earning after it has been disabled.
-        //       This line will be removed in the future.
-        if (wasEarningEnabled()) revert EarningCannotBeReenabled();
-
-        uint128 currentMIndex_ = _currentMIndex();
-
-        _enableDisableEarningIndices.push(currentMIndex_);
+        emit EarningEnabled(enableMIndex = _currentMIndex());
 
         IMTokenLike(mToken).startEarning();
-
-        emit EarningEnabled(currentMIndex_);
     }
 
     /// @inheritdoc IWrappedMToken
@@ -213,29 +208,23 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
         if (_isThisApprovedEarner()) revert IsApprovedEarner(address(this));
         if (!isEarningEnabled()) revert EarningIsDisabled();
 
-        uint128 currentMIndex_ = _currentMIndex();
+        emit EarningDisabled(disableIndex = currentIndex());
 
-        _enableDisableEarningIndices.push(currentMIndex_);
+        delete enableMIndex;
 
         IMTokenLike(mToken).stopEarning();
-
-        emit EarningDisabled(currentMIndex_);
     }
 
     /// @inheritdoc IWrappedMToken
     function startEarningFor(address account_) external {
-        if (!isEarningEnabled()) revert EarningIsDisabled();
-
-        // NOTE: Use `currentIndex()` if/when upgrading to support `startEarningFor` while earning is disabled.
-        _startEarningFor(account_, _currentMIndex());
+        _startEarningFor(account_, currentIndex());
     }
 
     /// @inheritdoc IWrappedMToken
     function startEarningFor(address[] calldata accounts_) external {
         if (!isEarningEnabled()) revert EarningIsDisabled();
 
-        // NOTE: Use `currentIndex()` if/when upgrading to support `startEarningFor` while earning is disabled.
-        uint128 currentIndex_ = _currentMIndex();
+        uint128 currentIndex_ = currentIndex();
 
         for (uint256 index_; index_ < accounts_.length; ++index_) {
             _startEarningFor(accounts_[index_], currentIndex_);
@@ -314,9 +303,9 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
 
     /// @inheritdoc IWrappedMToken
     function currentIndex() public view returns (uint128 index_) {
-        if (isEarningEnabled()) return _currentMIndex();
+        uint128 disableIndex_ = disableIndex == 0 ? IndexingMath.EXP_SCALED_ONE : disableIndex;
 
-        return UIntMath.max128(IndexingMath.EXP_SCALED_ONE, _lastDisableEarningIndex());
+        return enableMIndex == 0 ? disableIndex_ : (disableIndex_ * _currentMIndex()) / enableMIndex;
     }
 
     /// @inheritdoc IWrappedMToken
@@ -326,12 +315,7 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
 
     /// @inheritdoc IWrappedMToken
     function isEarningEnabled() public view returns (bool isEnabled_) {
-        return _enableDisableEarningIndices.length % 2 == 1;
-    }
-
-    /// @inheritdoc IWrappedMToken
-    function wasEarningEnabled() public view returns (bool wasEarning_) {
-        return _enableDisableEarningIndices.length != 0;
+        return enableMIndex != 0;
     }
 
     /// @inheritdoc IWrappedMToken
@@ -786,11 +770,6 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
             IRegistrarLike(registrar).listContains(EARNERS_LIST_NAME, address(this));
     }
 
-    /// @dev Returns the earning index from the last `disableEarning` call.
-    function _lastDisableEarningIndex() internal view returns (uint128 index_) {
-        return wasEarningEnabled() ? _unsafeAccess(_enableDisableEarningIndices, 1) : 0;
-    }
-
     /**
      * @dev    Compute the yield given an account's balance, earning principal, and the current index.
      * @param  balance_          The token balance of an earning account.
@@ -867,24 +846,5 @@ contract WrappedMToken is IWrappedMToken, Migratable, ERC20Extended {
      */
     function _revertIfInvalidRecipient(address account_) internal pure {
         if (account_ == address(0)) revert InvalidRecipient(account_);
-    }
-
-    /**
-     * @dev   Reads the uint128 value at some index of an array of uint128 values whose storage pointer is given,
-     *        assuming the index is valid, without wasting gas checking for out-of-bounds errors.
-     * @param array_ The storage pointer of an array of uint128 values.
-     * @param i_     The index of the array to read.
-     */
-    function _unsafeAccess(uint128[] storage array_, uint256 i_) internal view returns (uint128 value_) {
-        assembly {
-            mstore(0, array_.slot)
-
-            value_ := sload(add(keccak256(0, 0x20), div(i_, 2)))
-
-            // Since uint128 values take up either the top half or bottom half of a slot, shift the result accordingly.
-            if eq(mod(i_, 2), 1) {
-                value_ := shr(128, value_)
-            }
-        }
     }
 }
