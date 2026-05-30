a9e6efaf6401c34b19c0e509cce392c6fc11122d
khrafts <uwusebauyoyou@gmail.com>
2026-05-30 16:38:41 +0100

    chore: widen pragma 0.8.26 -> ^0.8.26 to admit Seismic ssolc 0.8.31

--- src/interfaces/IRegistrarLike.sol
+++ src/interfaces/IRegistrarLike.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 /**
  * @title  Subset of Registrar interface required for source contracts.
--- src/interfaces/IWrappedMToken.sol
+++ src/interfaces/IWrappedMToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 import { IERC20Extended } from "../../lib/common/src/interfaces/IERC20Extended.sol";
 
